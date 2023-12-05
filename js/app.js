0ee8ae3be6321608e3cedd1d4b20019d57a604e3
gustaellison <gustaellison@gmail.com>
2023-12-05 12:22:36 -0500

    working keyboard and different color cells

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
-import { allWords } from "./words.js"
+// import { allWords } from "./words.js"
+
+let allWords = ["SQUID"]
 
 const height = 6
 const width = 5
@@ -14,6 +16,8 @@ let gameOver = false
 
 // const generateNewWordEl = document.getElementById('new-word')
 const playAgainEl = document.getElementById('play-again')
+const keyboardEls = document.querySelectorAll('.keyboard > button')
+
 
 
 // selecting randomized word
@@ -53,11 +57,16 @@ document.addEventListener('keyup', letterEntry)
 let currTile;
 
 let guessedLetters = []
+let guessedWord;
 
 function letterEntry (e) {
     if(gameOver) return;
+    if (e.code === "Enter" && col !== 5){
+        return alert('Word is not long enough!')
+        
+    }
     
-    if ("KeyA" <= e.code && e.code <= "KeyZ") {
+    else if ("KeyA" <= e.code && e.code <= "KeyZ") {
         if (col < width) {
             currTile = document.getElementById(row.toString() + '-' + col.toString());
             if (currTile.innerText == "") {
@@ -73,11 +82,11 @@ function letterEntry (e) {
         currTile.innerText = ""
         
     } else if (e.code === "Enter" && col === 5){
-        update()
+        updateGameboard()
         row += 1
         col = 0
         getGuessedLetters()
-        
+        updateKeyboard()
         
     } if (!gameOver && row === height){
         gameOver = true
@@ -87,16 +96,108 @@ function letterEntry (e) {
     
 }
 
-function getGuessedLetters(){
-    allTilesEl.forEach ((tile)=>{
-        guessedLetters.push(tile.innerText)
-    })
-    console.log(guessedLetters)
+
+function getGuessedLetters() {
+    const newGuessedLetters = [];
+
+    allTilesEl.forEach((tile) => {
+        const letter = tile.innerText;
+        if (letter && !guessedLetters.includes(letter)) {
+            newGuessedLetters.push(letter);
+        }
+    });
+
+    // Add the new letters to the front of the guessedLetters array
+    guessedLetters = newGuessedLetters.concat(guessedLetters);
+
+    console.log(guessedLetters);
 }
 
 
+function updateKeyboard() {
+    for (let i = 0; i <= 26; i++) {
+        // Assuming guessedLetters is a string or an array of letters
+        const keyLetter = keyboardEls[i].innerText;
+        
+
+        // Remove all classes before applying the correct ones
+        keyboardEls[i].classList.remove('absent', 'correct', 'present');
+
+        if (guessedLetters.includes(keyLetter)) {
+            const wordIndex = guessedLetters.indexOf(keyLetter);
+            if (wordIndex !== -1 && word[wordIndex] === keyLetter) {
+                keyboardEls[i].classList.add('correct');
+                console.log(wordIndex)
+            } 
+            else if (word.includes(keyLetter)) {
+                keyboardEls[i].classList.add('present');
+            } 
+            else {
+                keyboardEls[i].classList.add('absent');
+            }
+        }
+    }
+}
+
+
+keyboardEls.forEach(key => key.addEventListener('click', keyboardTyping));
+
+function keyboardTyping (e){
+    if(gameOver) return;
+    console.log(e.target)
+    if (e.target.innerText === "Enter" && col !== 5){
+        return alert('Word is not long enough!')
+        
+    }
+    else if ("A" <= e.target.innerText && e.target.innerText <= "Z" && e.target.innerText !== 'Delete' && e.target.innerText !== 'Enter') {
+        if (col < width) {
+            currTile = document.getElementById(row.toString() + '-' + col.toString());
+            if (currTile.innerText == "" && e.target.innerText !== 'Delete') {
+                currTile.innerText = e.target.innerText;
+                col += 1;
+            }
+        }
+    } 
+    else if (e.target.innerText === "Delete"){
+        if(col > 0 && col <= width){
+            col = col - 1
+        }
+        currTile = document.getElementById(row.toString() + '-' + col.toString());
+        currTile.innerText = ""
+        
+    } 
+    else if (e.target.innerText === "Enter" && col === 5){
+        updateGameboard()
+        row += 1
+        col = 0
+        getGuessedLetters()
+        updateKeyboard()
+        
+    } if (!gameOver && row === height){
+        gameOver = true
+        document.getElementById('answer').innerText = word
+        showPlayAgain()
+    }
+    
+}
+
+// function updateKeyboard(){
+
+// }
+
+
+// function checkGuessedWord(){
+//     guessedWord = guessedLetters.join('')
+//     if (allWords.includes(guessedWord)){
+//         return
+//     } else {
+//        return  alert('not a valid word')
+//     }
+//     // console.log(guessedWord)
+// }
+
 // coloring the cell based on the guess
-function update(){
+function updateGameboard(){
     let correct = 0
     for (let c = 0; c < width; c++) {
         let currTile = document.getElementById(row.toString() + '-' + c.toString());
@@ -122,17 +223,17 @@ function update(){
 
 
 
-
+// displaying Play again button after gameOver is true
 function showPlayAgain(){
     if (gameOver === true){
         playAgainEl.style.display = 'inline-flex'
     }
 }
 
+// play again button/function
 document.addEventListener('click', resetGameBoard)
 
 function resetGameBoard(e){
-    console.log(e)
     if (e.target.id === 'play-again'){
         document.getElementById('board').replaceChildren()
         playAgainEl.style.display = 'none'
@@ -142,18 +243,11 @@ function resetGameBoard(e){
         gameOver = false
         word = allWords[Math.floor(Math.random()*allWords.length)].toLocaleUpperCase()
         document.getElementById('answer').innerText = ''
+        guessedLetters =[]
+        
+            keyboardEls.forEach((key) => {
+                key.setAttribute('class', '')
+            })
 
     }
 }
-
-//
-
-
-
-
-
-// win logic
-
-// score count function
-
-// play again button/function
